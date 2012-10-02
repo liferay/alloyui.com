8dee52c3daca200e55f1b19789fe35312e1f13f9
Zeno Rocha <zno.rocha@gmail.com>
2012-10-02 20:07:32 -0300

    adding a new way to deal with examples

--- _plugins/examples.rb
+++ _plugins/examples.rb
@@ -1,5 +1,5 @@
 module Jekyll
-  class ListDemoIndex < Page
+  class ListExampleIndex < Page
     def initialize(site, base, dir)
       @site = site
       @base = base
@@ -16,18 +16,17 @@ module Jekyll
         Dir['examples/*.yml'].each do |path|
           name   = File.basename(path, '.yml')
           config = YAML.load(File.read(File.join(@base, path)))
-          type   = config['type']
 
           if config['active']
-            example[type] = {} if example[type].nil?
-            example[type][name] = config
+            example[name] = {} if example[name].nil?
+            example[name] = config
           end
         end
       end
     end
   end
 
-  class SingleDemoIndex < Page
+  class SingleExampleIndex < Page
     def initialize(site, base, dir, path)
       @site     = site
       @base     = base
@@ -40,7 +39,7 @@ module Jekyll
     end
   end
 
-  class GenerateListDemo < Generator
+  class GenerateListExample < Generator
     safe true
     priority :normal
 
@@ -54,7 +53,7 @@ module Jekyll
         Dir.chdir('examples')
         Dir["*.yml"].each do |path|
           name = File.basename(path, '.yml')
-          self.write_single_demo_index(site, "examples/#{path}", name)
+          self.write_single_example_index(site, "examples/#{path}", name)
         end
 
         Dir.chdir(site.source)
@@ -63,7 +62,7 @@ module Jekyll
     end
 
     def write_example_index(site)
-      example = ListDemoIndex.new(site, site.source, "/examples")
+      example = ListExampleIndex.new(site, site.source, "/examples")
       example.render(site.layouts, site.site_payload)
       example.write(site.dest)
 
@@ -71,15 +70,15 @@ module Jekyll
       site.static_files << example
     end
 
-    def write_single_demo_index(site, path, name)
-      single_demo = SingleDemoIndex.new(site, site.source, "/examples/#{name}", path)
+    def write_single_example_index(site, path, name)
+      single_example = SingleExampleIndex.new(site, site.source, "/examples/#{name}", path)
 
-      if single_demo.data['active']
-        single_demo.render(site.layouts, site.site_payload)
-        single_demo.write(site.dest)
+      if single_example.data['active']
+        single_example.render(site.layouts, site.site_payload)
+        single_example.write(site.dest)
 
-        site.pages << single_demo
-        site.static_files << single_demo
+        site.pages << single_example
+        site.static_files << single_example
       end
     end
   end
