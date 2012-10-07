5cefbfbeee015a6d60fe5bdde6d0bab248eea60a
Zeno Rocha <zno.rocha@gmail.com>
2012-10-07 14:13:28 -0300

    adding a step to install dependencies

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
+# Install dependencies: npm install .
 # Build the website locally: docpad generate
 # Go to the gh-pages branch: git checkout gh-pages
 # Remove all files under version control: git rm -rf .
@@ -6,4 +7,4 @@
 # Commit changes to gh-pages branch: git add . && git commit -m "Regenerate" && git push origin gh-pages
 # Goes back to master branch: git checkout master
 
-docpad generate && git checkout gh-pages && git rm -rf . && mv out/* . && rm -rf out && git add . && git commit -m "Regenerate" && git push origin gh-pages && git checkout master
+npm install . && docpad generate && git checkout gh-pages && git rm -rf . && mv out/* . && rm -rf out && git add . && git commit -m "Regenerate" && git push origin gh-pages && git checkout master
