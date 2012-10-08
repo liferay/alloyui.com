0e74ff66bb3b593a6f388dcf5b4876af4db62baa
Zeno Rocha <zno.rocha@gmail.com>
2012-10-08 13:29:29 -0300

    removing node_modules

--- publish.sh
+++ publish.sh
@@ -4,7 +4,8 @@
 # Remove all files under version control: git rm -rf .
 # Move files from folder out to root: mv out/* .
 # Removing out folder: rm -rf out
+# Removing node_modules folder: rm -rf node_modules
 # Commit changes to gh-pages branch: git add . && git commit -m "Regenerate" && git push origin gh-pages
 # Goes back to master branch: git checkout master
 
-npm install . && docpad generate && git checkout gh-pages && git rm -rf . && mv out/* . && rm -rf out && git add . && git commit -m "Regenerate" && git push origin gh-pages && git checkout master
+npm install . && docpad generate && git checkout gh-pages && git rm -rf . && mv out/* . && rm -rf out && rm -rf node_modules && git add . && git commit -m "Regenerate" && git push origin gh-pages && git checkout master
