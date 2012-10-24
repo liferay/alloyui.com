529d24326fe0b7fe477a5ffb8133be894f19a1fa
Zeno Rocha <zno.rocha@gmail.com>
2012-10-24 00:59:31 -0200

    changing generate parameter

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 # Install dependencies: npm install .
-# Build the website locally: docpad generate
+# Build the website locally: docpad generate --env static
 # Go to the gh-pages branch: git checkout gh-pages
 # Remove all files under version control: git rm -rf .
 # Move files from folder out to root: mv out/* .
@@ -8,4 +8,4 @@
 # Commit changes to gh-pages branch: git add . && git commit -m "Regenerate" && git push origin gh-pages
 # Goes back to master branch: git checkout master
 
-npm install . && docpad generate && git checkout gh-pages && git rm -rf . && mv out/* . && rm -rf out && rm -rf node_modules && git add . && git commit -m "Regenerate" && git push origin gh-pages && git checkout master
+npm install . && docpad generate --env static && git checkout gh-pages && git rm -rf . && mv out/* . && rm -rf out && rm -rf node_modules && git add . && git commit -m "Regenerate" && git push origin gh-pages && git checkout master
