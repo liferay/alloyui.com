e0e976139c56195be645b1a004a2118966788f63
Zeno Rocha <zno.rocha@gmail.com>
2012-10-30 02:17:26 -0200

    basic io example working again

---
+++ src/documents/examples/io/basic.js
@@ -0,0 +1,15 @@
+  AUI().use('io', function (A) {
+
+    // Make an HTTP request to this URI.
+    var request = A.io("assets/content.html");
+
+    // Subscribe to event "io:complete" and define an event handler "complete".
+    A.on('io:complete', complete);
+
+    // Define a function to handle the response data.
+    function complete(id, out, args) {
+        var data = out.responseText;
+        alert(data);
+    };
+
+  });
