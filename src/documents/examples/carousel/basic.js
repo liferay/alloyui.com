d17d87c20eecf53a8531c1c6a678f1403e3fe1c9
Zeno Rocha <zno.rocha@gmail.com>
2012-10-31 16:35:23 -0200

    removing unnecessary code

--- src/documents/examples/carousel/basic.js
+++ src/documents/examples/carousel/basic.js
@@ -1,14 +1,9 @@
 AUI().ready('aui-carousel', function(A) {
 
   var carousel = new A.Carousel({
-      intervalTime: 2,
       contentBox: '#demo',
-      activeIndex: 0,
-      // activeIndex: 'rand',
-      height: 254,
-      width: 680
-      // nodeMenu: '#customMenu',
-      // nodeMenuItemSelector: 'li'
+      width: 680,
+      height: 254
   }).render();
 
 });
