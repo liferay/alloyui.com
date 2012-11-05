fe7f965876fbfc1b4f719c122ae83593d749918d
Zeno Rocha <zno.rocha@gmail.com>
2012-11-05 18:35:17 -0200

    removing alloy-ui folder #35

--- src/files/js/alloy-ui/aui-arraysort/aui-arraysort-min.js
+++
@@ -1,1 +0,0 @@
-AUI.add("aui-arraysort",function(a){ASort=a.ArraySort;a.mix(ASort,{compareIgnoreWhiteSpace:function(d,c,g,f){var e;f=f||ASort.compare;if((d==="")&&(c==="")){e=0;}else{if(d===""){e=1;}else{if(c===""){e=-1;}else{e=f.apply(this,arguments);}}}return e;}});},"@VERSION@",{skinnable:false,requires:["arraysort"]});
