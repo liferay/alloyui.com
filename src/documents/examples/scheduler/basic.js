a3abcf7a6b2efe2bb8db08c9d5cf7ee3a7043745
Zeno Rocha <zno.rocha@gmail.com>
2012-11-21 16:16:30 -0200

    adding schedule basic example #56

--- src/documents/examples/scheduler/basic.js
+++ src/documents/examples/scheduler/basic.js
@@ -1,27 +1,24 @@
 AUI().use('aui-scheduler', function(A) {
 
-  var events1 = [
+  var events = [
     {
-      content: 'Large Event 1',
-      endDate: new Date(2010, 9, 13, 12),
-      startDate: new Date(2010, 9, 13, 9)
+      content: 'Soccer Game',
+      startDate: new Date(2012, 9, 10, 2),
+      endDate: new Date(2012, 9, 10, 6)
     }
-  ];
+  ],
 
-  var dayView = new A.SchedulerDayView();
-  var monthView = new A.SchedulerMonthView();
-  var weekView = new A.SchedulerWeekView();
+  dayView = new A.SchedulerDayView(),
+  monthView = new A.SchedulerMonthView(),
+  weekView = new A.SchedulerWeekView(),
 
-  var eventRecorder = new A.SchedulerEventRecorder({
-    duration: 30
-  });
+  eventRecorder = new A.SchedulerEventRecorder(),
 
-  var scheduler = new A.Scheduler({
+  scheduler = new A.Scheduler({
     boundingBox: '#scheduler',
-    date: new Date(2010, 9, 12),
+    date: new Date(2012, 9, 12),
     eventRecorder: eventRecorder,
-    items: events1,
-    firstDayOfWeek: 1,
+    items: events,
     render: true,
     views: [weekView, dayView, monthView]
   });
