587774f9ba14c251e19367f5d0fea77e7599e21a
Zeno Rocha <zno.rocha@gmail.com>
2012-11-22 17:34:58 -0200

    adding basic example for dialog module

---
+++ src/documents/examples/dialog/basic.js
@@ -0,0 +1,9 @@
+AUI().use('aui-dialog', function(A) {
+
+  var dialog = new A.Dialog({
+    bodyContent: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    title: 'Dialog title',
+    centered: true
+ }).render();
+
+});
