92ba34a1505291881025394951f37068a4af7e4d
Zeno Rocha <zno.rocha@gmail.com>
2012-12-21 04:34:40 -0200

    source format for basic #83

--- src/documents/examples/node/basic.js
+++ src/documents/examples/node/basic.js
@@ -1,10 +1,10 @@
 AUI().use('aui-node', function(A) {
-  var node = A.one('.toggleColor');
+
+  var node = A.one('#toggleColor');
 
   node.on('click', function() {
-
-    var randomColor = Math.floor(Math.random()*16777215).toString(16);
-
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     node.setStyle('background', '#' + randomColor);
   });
+
 });
