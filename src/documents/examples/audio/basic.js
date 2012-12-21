ff4cc640dddc8e1d8f98700d65f4953f69d3b4d1
Zeno Rocha <zno.rocha@gmail.com>
2012-12-21 00:53:22 -0200

    source format #85

--- src/documents/examples/audio/basic.js
+++ src/documents/examples/audio/basic.js
@@ -1,7 +1,8 @@
 AUI().use('aui-audio', function(A) {
 
-  new A.Audio({
-      boundingBox: '#demo',
-      url: 'http://66.90.118.45/ost/zelda-1/filyhgqlhs/02-overworld.mp3'
-    }).render();
+  var audio = new A.Audio({
+    boundingBox: '#demo',
+    url: 'http://66.90.118.45/ost/zelda-1/filyhgqlhs/02-overworld.mp3'
+  }).render();
+
 });
