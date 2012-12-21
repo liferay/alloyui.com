b6d046883ef6c4de0d1042e74b1917bec8e361b2
Zeno Rocha <zno.rocha@gmail.com>
2012-12-21 03:53:21 -0200

    source format #82

--- src/documents/examples/form-validator/basic.js
+++ src/documents/examples/form-validator/basic.js
@@ -1,7 +1,7 @@
 AUI().ready('aui-form-validator', function(A) {
 
-  var validator1 = new A.FormValidator({
-    boundingBox: '#fm1'
+  var validator = new A.FormValidator({
+    boundingBox: '#form'
   });
 
 });
