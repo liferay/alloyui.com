1f8df1854356cfb8041466cec612a2b6215061b0
patrick_armitage <patrick.armitage@liferay.com>
2012-12-20 16:35:23 -0800

    Add Toggler Examples.

---
+++ src/documents/examples/toggler/basic.js
@@ -0,0 +1,9 @@
+AUI().use('aui-toggler', function(A) {
+
+	var toggler = new A.Toggler({
+		container: "#myToggler",
+		content: ".content",
+		header: ".header",
+		expanded: false
+	});
+});
---
+++ src/documents/examples/toggler/real-world.js
@@ -0,0 +1,15 @@
+AUI().use('aui-toggler', function(A) {
+
+	var togglerDelegate = new A.TogglerDelegate({
+		animated: true,
+		closeAllOnExpand: true,
+		container: "#togglerDelegate1",
+		content: '.content',
+		expanded: false,
+		header: '.header',
+		transition: {
+			duration: .2,
+			easing: 'cubic-bezier'
+		}
+	});
+});
