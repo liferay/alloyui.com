6c1a805d64b3b01e749c0ebb116611a7afb073ad
Zeno Rocha <zno.rocha@gmail.com>
2012-12-27 16:04:10 -0200

    changed indentation from tabs to space

--- src/documents/examples/toggler/basic.js
+++ src/documents/examples/toggler/basic.js
@@ -1,9 +1,10 @@
 AUI().use('aui-toggler', function(A) {
 
-	var toggler = new A.Toggler({
-		container: "#myToggler",
-		content: ".content",
-		header: ".header",
-		expanded: false
-	});
+  var toggler = new A.Toggler({
+    container: "#myToggler",
+    content: ".content",
+    header: ".header",
+    expanded: false
+  });
+
 });
