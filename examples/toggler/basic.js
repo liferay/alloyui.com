3c2654ebd8cb0612dd3de515a4ad6de5f6c9e7c2
Zeno Rocha <zno.rocha@gmail.com>
2012-12-28 15:14:05 -0200

    Regenerate

---
+++ examples/toggler/basic.js
@@ -0,0 +1,10 @@
+AUI().use('aui-toggler', function(A) {
+
+  var toggler = new A.Toggler({
+    container: "#myToggler",
+    content: ".content",
+    header: ".header",
+    expanded: false
+  });
+
+});
