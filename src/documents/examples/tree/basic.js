5d0a09df8b5fbc54a330e93c79b802bb7e132d98
Marc Lundgren <marc.lundgren@liferay.com>
2013-01-07 17:28:40 -0800

    Alloyui.com - Example - TreeView - source formatting

--- src/documents/examples/tree/basic.js
+++ src/documents/examples/tree/basic.js
@@ -1,17 +1,21 @@
 AUI().use('aui-tree-view', function(A) {
 
-  var children =  [{
+  // // Create an array object for the tree root and child nodes
+  var children = [{
     label: 'ROOT',
-    children: [{
-      label: 'Folder1',
-      children: [{ label: 'File' }]
-    },
-    {
-      label: 'Folder2',
-      children: [{ label: 'File' }]
-    }]
-  }]
+    children: [
+      {
+        label: 'Folder1',
+        children: [{ label: 'File' }]
+      },
+      {
+        label: 'Folder2',
+        children: [{ label: 'File' }]
+      }
+    ]
+  }];
 
+  // Create a TreeView Component
   var treeView = new A.TreeView({
     boundingBox: '#myTreeView',
     children: children
--- src/documents/examples/tree/real-world.js
+++ src/documents/examples/tree/real-world.js
@@ -1,41 +1,89 @@
 AUI().use('aui-tree-view', function(A) {
-  // Create TreeView
-  var cacheChildren = [{
-    type: 'io',
-    label: 'ROOT',
-    id: 'root',
-    expanded: true,
-    cache: true,
-    io: 'assets/content.html'
-  }]
 
-  var treeView = new A.TreeViewDD({
-    height: '380',
-    boundingBox: '#myTreeView',
-    children: cacheChildren
-  }).render();
+  // Define our real world example
+  var RealWorldExample = {
 
-  // Button Event Assignment
-  var ROOT = treeView.getNodeById('root');
+    init: function() {
+      var myTreeView = A.one('#myTreeView');
 
-  A.on('click', function() {
-    var tempNode = ROOT.createNode({
-      label: 'Temp folder',
-      children: [{ label: 'Temp file' }]
-    });
+      var loadingmask = this.createLoadingMask(myTreeView);
 
-    ROOT.appendChild(tempNode);
-  }, '#createNodeROOT');
+      var treeView = this.createTreeMyView();
 
-  A.on('click', function() {
-    var tempNode = ROOT.createNode({
-      label: 'Temp folder',
-      children: [{ label: 'Temp file' }]
-    });
+      treeView.on('render', function(event) {
+        loadingmask.hide();
+      });
+    },
 
-    ROOT.insertBefore(tempNode);
-  }, '#insertBeforeROOT');
+    createLoadingMask: function(node) {
+      node.plug(A.LoadingMask);
 
-  A.on('click', function() { treeView.expandAll(); }, '#expandAll');
-  A.on('click', function() { treeView.collapseAll(); }, '#collapseAll');
+      var loadingmask = node.loadingmask;
+
+      loadingmask.show();
+
+      return loadingmask;
+    },
+    createTreeMyView: function() {
+
+      // Create an array object for the root node
+      var root = [{
+        type: 'io',
+        label: 'ROOT',
+        id: 'root',
+        expanded: true,
+        cache: true,
+        io: 'assets/content.html'
+      }];
+
+      // Create an array object for temp files child nodes
+      var tempFile = [{ label: 'Temp file' }];
+
+      // Create a TreeView Component with Drag & Drop support
+      var treeView = new A.TreeViewDD({
+        height: '380',
+        boundingBox: '#myTreeView',
+        children: root
+      }).render();
+
+      // Attach button event listeners
+      var ROOT = treeView.getNodeById('root');
+
+      var createRootTempNode = function() {
+        return ROOT.createNode({
+          label: 'Temp folder',
+          children: tempFile
+        });
+      };
+
+      A.on(
+        'click',
+        function() {
+          var tempNode = createRootTempNode();
+
+          ROOT.appendChild(tempNode);
+        },
+        '#createNodeROOT'
+      );
+
+      A.on(
+        'click',
+        function() {
+          var tempNode = createRootTempNode();
+
+          ROOT.insertBefore(tempNode);
+        },
+        '#insertBeforeROOT'
+      );
+
+      A.on('click', function() { treeView.expandAll(); }, '#expandAll');
+      A.on('click', function() { treeView.collapseAll(); }, '#collapseAll');
+
+      return treeView;
+    },
+  };
+
+  // Run our real world example
+  RealWorldExample.init();
+
 });
