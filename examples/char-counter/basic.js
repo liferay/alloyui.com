08df56b9bf9cb3c0d0c4d59fb70dfd1bfbc3f964
Zeno Rocha <zno.rocha@gmail.com>
2013-01-18 20:33:49 -0200

    Regenerate

--- examples/char-counter/basic.js
+++ examples/char-counter/basic.js
@@ -1,14 +1,9 @@
 AUI().ready('aui-char-counter', function(A) {
 
   var cc = new A.CharCounter({
-    input: '#input',
-    counter: '#counter',
-    maxLength: 10,
-    on: {
-      maxLength: function(event) {
-        alert('The max length limit was reached');
-      }
-    }
+    input: '#myInput',
+    counter: '#myCounter',
+    maxLength: 10
   });
 
 });
