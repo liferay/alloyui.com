3463f04440332ac796a1663b8d767743f633d9f0
Marc Lundgren <marc.lundgren@liferay.com>
2013-01-23 16:49:23 -0800

    Alloyui.com - Example - Template - source format

--- src/documents/examples/template/basic.js
+++ src/documents/examples/template/basic.js
@@ -1,14 +1,30 @@
 AUI().use('aui-template', function(A) {
-  var objects = [
-    { key: 'AUI', value: 'AlloyUI', auth: 'Designed by Liferay' },
-    { key: 'JQ', value: 'jQuery', auth: 'Designed by Team jQuery' },
-    { key: 'JS', value: 'Javascript', auth: 'Designed by Brendan Eich' },
-    { key: 'YUI', value: 'YahooUI', auth: 'Designed by Yahoo!' }
-  ]
+  var projects = [
+    {
+      abbreviation: 'AUI',
+      name: 'AlloyUI',
+      designer: 'Liferay'
+    },
+    {
+      abbreviation: 'JQ',
+      name: 'jQuery',
+      designer: 'Team jQuery'
+    },
+    {
+      abbreviation: 'JS',
+      name: 'Javascript',
+      designer: 'Brendan Eich'
+    },
+    {
+      abbreviation: 'YUI',
+      name: 'YahooUI',
+      designer: 'Yahoo!'
+    }
+  ];
 
-  tpl = new A.Template.from('#myTemplate');
+  var tpl = new A.Template.from('#myTemplate');
 
   A.one("#render").on('click', function(event) {
-      tpl.render(objects, '#myTemplate');
+      tpl.render(projects, '#myTemplate');
   });
 });
--- src/documents/examples/template/real-world.js
+++ src/documents/examples/template/real-world.js
@@ -1,12 +1,36 @@
 AUI().use('aui-template', function(A) {
   var foods = [
-    { food: 'Carrots', taste: 'Yuck', nutr: 'B+', weight: 'None', rating: 'Low' },
-    { food: 'Steak', taste: 'Yummy', nutr: 'B-', weight: 'Large', rating: 'High' },
-    { food: 'Creme Brule', taste: 'Incremebulous', nutr: 'C-', weight: 'Extreme', rating: '6/5' },
-    { food: 'Beets', taste: 'Rooty', nutr: 'A++', weight: '-5', rating: 'Stay Away' }
-  ]
+    {
+      food: 'Carrots',
+      taste: 'Yuck',
+      nutr: 'B+',
+      weight: 'None',
+      rating: 'Low'
+    },
+    {
+      food: 'Steak',
+      taste: 'Yummy',
+      nutr: 'B-',
+      weight: 'Large',
+      rating: 'High'
+    },
+    {
+      food: 'Creme Brule',
+      taste: 'Incremebulous',
+      nutr: 'C-',
+      weight: 'Extreme',
+      rating: '6/5'
+    },
+    {
+      food: 'Beets',
+      taste: 'Rooty',
+      nutr: 'A++',
+      weight: '-5',
+      rating: 'Stay Away'
+    }
+  ];
 
-  tpl = new A.Template.from('#auiScriptTPL');
+  var tpl = new A.Template.from('#auiScriptTPL');
 
   A.one("#render").on('click', function(event) {
       tpl.render(foods, '#myTemplate');
