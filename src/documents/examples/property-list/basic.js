e1df5abddcb7d10377c48c7fae3ac8d97022082b
Zeno Rocha <zno.rocha@gmail.com>
2013-01-24 16:01:40 -0200

    json compacted #113

--- src/documents/examples/property-list/basic.js
+++ src/documents/examples/property-list/basic.js
@@ -1,13 +1,8 @@
 AUI().use('aui-property-list', function(A) {
+
   var properties = [
-    {
-      name: 'Fruit',
-      value: 'Strawberry'
-    },
-    {
-      name: 'Veggie',
-      value: 'Broccoli'
-    }
+    { name: 'Fruit',  value: 'Strawberry' },
+    { name: 'Veggie', value: 'Broccoli' }
   ];
 
   new A.PropertyList({
