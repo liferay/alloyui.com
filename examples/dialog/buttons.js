245462fdf55362f2c008aa73a5f88df9e1a37934
Zeno Rocha <zno.rocha@gmail.com>
2013-01-25 15:07:55 -0200

    Regenerate

---
+++ examples/dialog/buttons.js
@@ -0,0 +1,25 @@
+AUI().use('aui-dialog', function(A) {
+
+  var myButtons = [
+    {
+      label: 'Change title',
+      handler: function() {
+        this.set('title', 'New dialog title');
+      }
+    },
+    {
+      label: 'Change body',
+      handler: function() {
+        this.set('bodyContent', 'New body content');
+      }
+    }
+  ],
+
+  new A.Dialog({
+    bodyContent: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    title: 'Dialog title',
+    buttons: myButtons,
+    centered: true
+  }).render();
+
+});
