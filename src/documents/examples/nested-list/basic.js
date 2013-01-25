0413f74a1d36aae4799c50aebf8e559d1beacaf9
Zeno Rocha <zno.rocha@gmail.com>
2013-01-25 15:48:14 -0200

    improving nested list

--- src/documents/examples/nested-list/basic.js
+++ src/documents/examples/nested-list/basic.js
@@ -2,7 +2,7 @@ AUI().ready('aui-nested-list', function(A) {
 
   var placeholder = A.Node.create('<li class="placeholder"></li>');
 
-  var list = new A.NestedList({
+  new A.NestedList({
     nodes: '#myList li',
     dropCondition: function(event) {
       return true;
