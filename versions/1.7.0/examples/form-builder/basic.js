64009ed4f42d607eedb77f13579374bf29b0c5ff
Zeno Rocha <zno.rocha@gmail.com>
2013-02-03 20:10:56 -0300

    Regenerate

---
+++ versions/1.7.0/examples/form-builder/basic.js
@@ -0,0 +1,89 @@
+AUI().use('aui-form-builder', function(A) {
+
+  new A.FormBuilder({
+    boundingBox: '#myFormBuilder',
+    availableFields: [
+      {
+        iconClass: 'aui-form-builder-field-icon-text',
+        label: 'Text',
+        type: 'text',
+        unique: true,
+        width: 75,
+        id: 'uniqueTextField',
+        readOnlyAttributes: ['name']
+      },
+      {
+        iconClass: 'aui-form-builder-field-icon-textarea',
+        label: 'Textarea',
+        type: 'textarea',
+        hiddenAttributes: ['tip']
+      },
+      {
+        iconClass: 'aui-form-builder-field-icon-checkbox',
+        label: 'Checkbox',
+        type: 'checkbox'
+      },
+      {
+        iconClass: 'aui-form-builder-field-icon-button',
+        label: 'Button',
+        type: 'button'
+      },
+      {
+        iconClass: 'aui-form-builder-field-icon-select',
+        label: 'Select',
+        type: 'select'
+      },
+      {
+        iconClass: 'aui-form-builder-field-icon-radio',
+        label: 'Radio Buttons',
+        type: 'radio'
+      },
+      {
+        iconClass: 'aui-form-builder-field-icon-fileupload',
+        label: 'File Upload',
+        type: 'fileupload'
+      },
+      {
+        iconClass: 'aui-form-builder-field-icon-fieldset',
+        label: 'Fieldset',
+        type: 'fieldset'
+      }
+    ],
+    fields: [
+      {
+        label: 'City',
+        predefinedValue: 'chicago',
+        type: 'select',
+        options: [
+          {
+            label: 'Ney York',
+            value: 'new york'
+          },
+          {
+            label: 'Chicago',
+            value: 'chicago'
+          }
+        ]
+      },
+      {
+        label: 'Colors',
+        type: 'radio',
+        options: [
+          {
+            label: 'Red',
+            value: 'red'
+          },
+          {
+            label: 'Green',
+            value: 'green'
+          },
+          {
+            label: 'Blue',
+            value: 'blue'
+          }
+        ]
+      }
+    ]
+  }).render();
+
+});
---
+++ versions/1.7.0/examples/tooltip/title.js
@@ -0,0 +1,8 @@
+AUI().ready('aui-tooltip', function(A) {
+
+  new A.Tooltip({
+    trigger: '#myTooltip',
+    title: true
+  }).render();
+
+});
