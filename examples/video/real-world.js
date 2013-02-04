86c1f70a16256ee2b97fa26aa3df33c169f8bb4d
Zeno Rocha <zno.rocha@gmail.com>
2013-02-04 09:08:27 -0300

    Regenerate

--- examples/video/real-world.js
+++ examples/video/real-world.js
@@ -2,12 +2,12 @@ YUI().ready('aui-video', function(Y) {
 
   new Y.Video({
     boundingBox: '#myVideo',
-    width: 640,
-    height: 368,
     url: 'http://videos.liferay.com/webinars/2010-08-11.mp4',
     ogvUrl: 'http://videos.liferay.com/lifecasts/portal/6.0/106.ogv',
     swfUrl: 'http://videos.liferay.com/common/player.swf',
-    poster: 'assets/sample-jpg.jpg',
+    poster: 'http://alloyui.com/video/poster.png',
+    width: 640,
+    height: 368,
     fixedAttributes: {
       allowfullscreen: 'true'
     }
