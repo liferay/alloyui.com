0482227a86718d55e65344eeacc02d6ee0bbf552
Zeno Rocha <zno.rocha@gmail.com>
2013-02-04 18:04:39 -0300

    Regenerate

---
+++ examples/toolbar/real-world.js
@@ -0,0 +1,32 @@
+YUI().use('aui-toolbar', function(Y) {
+
+  new Y.Toolbar({
+    boundingBox: '#myToolbar',
+    children: [
+      [
+        { icon: 'aui-icon-camera' },
+        { icon: 'aui-icon-print' }
+      ],
+      [
+        'radio',
+        { label: '1' },
+        { label: '2' },
+        { label: '3' }
+      ],
+      [
+        'checkbox',
+        { icon: 'aui-icon-bold' },
+        { icon: 'aui-icon-italic' },
+        { icon: 'aui-icon-font' }
+      ],
+      [
+        'radio',
+        { icon: 'aui-icon-align-left' },
+        { icon: 'aui-icon-align-center' },
+        { icon: 'aui-icon-align-right' },
+        { icon: 'aui-icon-align-justify' }
+      ]
+    ]
+  }).render();
+
+});
