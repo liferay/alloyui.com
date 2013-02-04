01b0582969f4b7f1a151287d5fcc3c1f6c19f1d0
Zeno Rocha <zno.rocha@gmail.com>
2013-02-04 16:55:40 -0300

    migrated button

--- src/documents/examples/button/basic.js
+++ src/documents/examples/button/basic.js
@@ -2,24 +2,14 @@ YUI().use('aui-button', function(Y) {
 
   new Y.Button({
     label: 'Basic',
-    srcNode: '#basic',
+    srcNode: '#myButton',
     icon: 'aui-icon-print',
     iconAlign: 'left'
   }).render();
 
   new Y.ToggleButton({
     label: 'Click to toggle',
-    srcNode: '#toggle'
-  }).render();
-
-  new Y.ButtonGroup({
-    boundingBox: '#checkgroup',
-    type: 'checkbox'
-  }).render();
-
-  new Y.ButtonGroup({
-    boundingBox: '#radiogroup',
-    type: 'radio'
+    srcNode: '#myToggleButton'
   }).render();
 
 });
