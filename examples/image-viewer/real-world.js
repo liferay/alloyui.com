d8900fbe62ab462722cfe5c9e8267fc6c219cc2e
Zeno Rocha <zno.rocha@gmail.com>
2013-03-08 14:50:47 -0300

    Regenerate

---
+++ examples/image-viewer/real-world.js
@@ -0,0 +1,20 @@
+YUI().use(
+  'aui-image-viewer-base',
+  function(Y) {
+    new Y.ImageViewer(
+      {
+        links: '#myGallery a',
+        anim: true,
+        caption: 'Liferay Champion Soccer',
+        captionFromTitle: true,
+        lockScroll: true,
+        preloadAllImages: true,
+        showInfo: true,
+        imageAnim: {
+         duration: 1,
+         easing: 'easeIn',
+        }
+      }
+    ).render();
+  }
+);
