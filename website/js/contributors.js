7d780c08c1d3a48482cda69e88bb1a7f0128e4ce
Zeno Rocha <zno.rocha@gmail.com>
2013-08-16 12:00:05 -0300

    Regenerate

--- website/js/contributors.js
+++ website/js/contributors.js
@@ -1,12 +1,10 @@
-YUI().use('jsonp', 'node',function (Y) {
-
+YUI().use('jsonp', 'node', 'aui-tooltip', function(Y) {
   var githubAPI = 'https://api.github.com/repos/liferay/alloy-ui/contributors?callback={callback}',
       template  = '<a href="http://github.com/{login}" title="@{login}">' +
                   '  <img src="{avatar_url}" width="50" height="50" alt="@{login}">' +
                   '</a>';
 
   function handleJSONP(response) {
-
     var contributorsHTML = '';
 
     for (var i = 0; i < response.data.length; i++) {
@@ -15,8 +13,11 @@ YUI().use('jsonp', 'node',function (Y) {
 
     Y.one("#contributors").setHTML(contributorsHTML);
 
+    new Y.TooltipDelegate({
+        trigger: '#contributors a',
+        zIndex: 1
+    });
   }
 
   Y.jsonp(githubAPI, handleJSONP);
-
 });
