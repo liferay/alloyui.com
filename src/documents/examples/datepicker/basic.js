0ea6c74d9a2e93d0e7b6a765443a1d7e9d0c9aaa
Zeno Rocha <zno.rocha@gmail.com>
2013-09-10 16:55:59 -0300

    Add datepicker demos

---
+++ src/documents/examples/datepicker/basic.js
@@ -0,0 +1,18 @@
+YUI().use(
+  'aui-datepicker',
+  function(Y) {
+    new Y.DatePicker(
+      {
+        trigger: 'button',
+        popover: {
+          zIndex: 1
+        },
+        on: {
+          selectionChange: function(event) {
+            console.log(event.newSelection)
+          }
+        },
+      }
+    );
+  }
+);
