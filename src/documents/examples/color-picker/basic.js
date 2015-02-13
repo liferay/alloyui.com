10583229773e2e29f83cce4058cd7882a9e49979
weston_hancock <weston.hancock@liferay.com>
2015-02-13 12:42:44 -0800

    Remove old files

--- src/documents/examples/color-picker/basic.js
+++
@@ -1,6 +0,0 @@
-YUI().use(
-  'aui-color-palette',
-  function(Y) {
-    new Y.ColorPalette().render('#myColorPalette');
-  }
-);
--- src/documents/examples/color-picker/hsv.js
+++
@@ -1,6 +0,0 @@
-YUI().use(
-  'aui-color-picker-base',
-  function(Y) {
-    new Y.HSVPalette().render('#myHsvPalette');
-  }
-);
